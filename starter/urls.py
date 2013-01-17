3c3757924ed8812b903c9c702c54b172f9c03969
Jim Munro <jim.munro@sendgrid.com>
2013-01-16 20:25:34 -0700

    starter complete

--- starter/urls.py
+++ starter/urls.py
@@ -1,7 +1,11 @@
 import os
 from django.conf.urls import patterns, include, url
-from starter.views import MyFormView, MyListView, MyUpdateView, MyDetailView
-from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from starter.models import Starter
+from django.views.generic.base import RedirectView
+from django.core.urlresolvers import reverse_lazy
 from django.conf import settings
 
 
@@ -10,16 +14,28 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^main-page/$', TemplateView.as_view(template_name="main_page.html")),
+    url(r'^$', RedirectView.as_view(url="main/"), name="root_url"),
+    (r'^main/$', 'starter.views.main_page'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
 # model form tests
-urlpatterns += patterns(
-    (r'^entry-form/$', MyFormView.as_view()),
-    (r'^list-forms/$', MyListView.as_view()),
-    url(r'^(?P<pk>\d+)/update/$', MyUpdateView.as_view(), name="update_form"),
-    url(r'^(?P<pk>\d+)/$', MyDetailView.as_view(), name="detail_view"),
+urlpatterns += patterns('',
+    url(r'^starter/$', ListView.as_view(model=Starter), name="list_page"),
+    url(r'^starter/create/$', CreateView.as_view(
+            model=Starter,
+            success_url=reverse_lazy('list_page')), 
+        name="create_page"),
+    url(r'^starter/(?P<pk>\d+)/update/$', UpdateView.as_view(
+            model=Starter, 
+            success_url=reverse_lazy('list_page')),
+        name="update_page"),
+    url(r'^starter/(?P<pk>\d+)/delete/$', DeleteView.as_view(
+            model=Starter, 
+            success_url = reverse_lazy('list_page'),
+            template_name_suffix="_confirm_delete"),
+        name="delete_page"),
+    url(r'^starter/(?P<pk>\d+)/$', DetailView.as_view(model=Starter), name="detail_page"),
     )
 
 ###################################
--- starter/views.py
+++ starter/views.py
@@ -1,32 +1,9 @@
 from django.http import HttpResponse, Http404
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse, reverse_lazy
 
-from starter.models import Starter
-from starter.forms import StarterForm
+from django.views.generic import TemplateView
 
-from django.views.generic import DetailView, TemplateView
-from django.views.generic.edit import FormView, CreateView, UpdateView
-from django.views.generic.list import ListView
-
-class MyFormView(CreateView):
-    template_name = 'starter_form.html'
-    form_class = StarterForm
-    success_url = '/list-page/'
-
-    def form_valid(self, form):
-        return super(MyFormView, self).form_valid(form)
-
-class MyUpdateView(UpdateView):
-	template_name='starter_form.html'
-	model = Starter
-	success_url = '/list-page/'
-
-class MyListView(ListView):
-	model = Starter
-	template_name = 'starter_list.html'
-
-class MyDetailView(DetailView):
-	model = Starter
-	template_name = 'starter_detail.html'
-
+def main_page(TemplateView):
+	template_name="main_page.html"
